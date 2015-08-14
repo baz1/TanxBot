7087be83dbb2bba235486d42d1c1c7f89c3d53bc
baz1 <baz1.github@gmail.com>
2015-08-14 15:29:20 +0200

    Argument parsing

--- main.cpp
+++ main.cpp
@@ -10,13 +10,60 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     if (!TanxMap::initialize())
         return 1;
-    QString name = "AAAA";
-    int team = 0;
+    QString tankName = "guest", followName = "", targetName = "";
+    int team = -1;
+    QStringList args = QCoreApplication::arguments();
+    foreach (const QString &arg, args)
+    {
+        if (arg.startsWith("name="))
+        {
+            tankName = arg.mid(5);
+            continue;
+        }
+        if (arg.startsWith("follow="))
+        {
+            followName = arg.mid(7);
+            continue;
+        }
+        if (arg.startsWith("target="))
+        {
+            targetName = arg.mid(7);
+            continue;
+        }
+        if (arg.startsWith("team="))
+        {
+            QString teamName = arg.mid(5).toLower();
+            if (teamName == "blue")
+            {
+                team = TEAM_BLUE;
+                continue;
+            }
+            if (teamName == "red")
+            {
+                team = TEAM_RED;
+                continue;
+            }
+            if (teamName == "green")
+            {
+                team = TEAM_GREEN;
+                continue;
+            }
+            if (teamName == "yellow")
+            {
+                team = TEAM_YELLOW;
+                continue;
+            }
+            fprintf(stderr, "Error: Unrecognized team \"%s\".\n", qPrintable(teamName));
+            return 1;
+        }
+        fprintf(stderr, "Error: Unrecognized option \"%s\".\n", qPrintable(arg));
+        return 1;
+    }
     while (true)
     {
         TanxInterface tanxThread(NULL, false);
         UserInterface userThread;
-        TanxPlayer tanxPlayer(&tanxThread, name, team);
+        TanxPlayer tanxPlayer(&tanxThread, tankName, followName, targetName, team);
         QEventLoop evtLoop;
         QObject::connect(&tanxThread, SIGNAL(disconnected()), &evtLoop, SLOT(quit()));
         QObject::connect(&userThread, SIGNAL(finished()), &tanxThread, SLOT(endConnection()));
